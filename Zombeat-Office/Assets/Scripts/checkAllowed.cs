4057dd7baf2dd4f8eef0fb19b1248b39d59066c8
Camille Briand <camille.briand.20160281@efrei.net>
2018-03-11 17:55:58 +0100

    Started a few scripts

---
+++ Zombeat-Office/Assets/Scripts/checkAllowed.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkAllowed : MonoBehaviour {
+
+    [HideInInspector] public bool exitAllowed = false;
+
+
+    public void allowExit()
+    {
+        exitAllowed = true;
+    }
+
+
+    public void disallowExit()
+    {
+        exitAllowed = false;
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If exit is allowed (level completed)
+        if (exitAllowed)
+        {
+            //TODO: implement level end
+        }
+    }
+}
