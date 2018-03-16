f8d1f22e1ee56c1008a9e8ac75d3c3ffd5a254fe
Camille Briand <camille.briand@efrei.net>
2018-03-16 15:03:18 +0100

    Made the placer scripts easier to use
    EN:
    - Added comments to both placer scripts (EnemiesPlacer.cs and LeversPlacer.cs)
    
    - Made both scripts look the same (only variables names may vary) so that they are easier to understand ==> Same amount of lines, same methods names, same instructions inside methods
    
    - Modified the Main scene to test the placer scripts (added empty Enemy and Levers gameobjects)
    
    ____________________________
    FR: 
    - Ajout de commentaires pour tous les scripts (EnemiesPlacer.cs and LeversPlacer.cs)
    
    - Mise en forme des scripts de façon à ce qu'ils aient la même apparence (seuls les noms de variables peuvent différer) ==> même nombre de lignes, mêmes noms de méthodes avec les mêmes instructions
    
    - Modifications apportées à la scène Main de façon à tester les scripts (ajout de GameObjects vides pour représenter les leviers et enemis potentiels)

--- Zombeat-Office/Assets/Scripts/EnemiesPlacer.cs
+++ Zombeat-Office/Assets/Scripts/EnemiesPlacer.cs
@@ -4,45 +4,96 @@ using UnityEngine;
 
 public class EnemiesPlacer : MonoBehaviour {
 
-    public List<GameObject> enemies;
-    public GameObject enabledEnemiesHolder;
-
+	[Header("Number of ennemies to spawn")]
+	public bool useMinMax = true;
     public int minimumToSpawn = 1;
     public int maximumToSpawn = 2;
 
-    public bool preciseNumber = false;
+    public bool usePreciseNumber = false;
     public int enemiesToSpawn = 0;
+
+	[Space(15)]
+	[Header("Enemies (Gameobjects)")]
+	[Space(5)]
+    public GameObject enabledEnemiesHolder;
+    public List<GameObject> enemies;
     
-	void Start () {
+
+    
+	void Start ()
+	{
+		if (!CheckParameters ())
+		{
+			Debug.Log ("Wrong parameters value(s), exiting the Start() method");
+			return;
+		}
+
+		// Disabling all potential enemies before enabling some of them back
         for (int i = 0; i < enemies.Count; i++)
             enemies[i].SetActive(false);
 
+		// Enabling some enemies
         SetEnemies();
 
+		// (Simple security) Disabling parent object of all potential enemies
         GameObject.Find("PotentialEnemies").SetActive(false);
+		// Activating enemies holders
         enabledEnemiesHolder.SetActive(true);
+	}
+
+    // Use this method to check the parameters' value(s)
+	private bool CheckParameters()
+	{
+        // Number of potential enemies
+		if (enemies.Count <= 0)
+        {
+			Debug.LogError (string.Format ("No potential levers were passed"));
+			return false;
+		}
+
+        // Precise number of enemies to spawn if using precise number
+		if (usePreciseNumber && (enemiesToSpawn < 0 || enemiesToSpawn > enemies.Count))
+        {
+			Debug.LogError (string.Format ("Cannot spawn {0} enemies with {1} item(s) passed", enemiesToSpawn, enemies.Count));
+			return false;
+		}
+
+        // Minimum and maximum enemies to spawn if not using precise number and using minimum and maximum
+        if (!usePreciseNumber && useMinMax && (minimumToSpawn < 0 || minimumToSpawn > enemies.Count || maximumToSpawn < 0 || maximumToSpawn > enemies.Count))
+		{
+			Debug.Log (string.Format ("Values passed for minimum and maximum enemies to spawn are not correct, setting them to 1 and {0} instead", enemies.Count));
+
+			minimumToSpawn = 1;
+			maximumToSpawn = enemies.Count;
+		}
+
+		return true;
 	}
 
 
     private void SetEnemies()
     {
-        int toSpawn = enemiesToSpawn;
-        if (!preciseNumber)
+        // Setting a proper number of enemies
+        int toSpawn = 0;
+        if (usePreciseNumber)
+            toSpawn = enemiesToSpawn;
+        else if (useMinMax)
             toSpawn = RandIntInRange(minimumToSpawn, maximumToSpawn);
+        else
+            toSpawn = RandIntInRange(1, enemies.Count);
 
-        if (toSpawn > enemies.Count)
-        {
-            Debug.LogError(string.Format("{0} enemies to spawn but there are only {1} potential enemies", toSpawn, enemies.Count));
-            return;
-        }
-
+        
+        // Activating the correct amount of enemies at random
         for (int i = 0; i < toSpawn; i++)
         {
+            // Getting a random subscript inside the enemies List
             int randomEnemyID = RandIntInRange(0, enemies.Count - 1);
 
+            // Adding the matching enemy to the enabledEnemiesHolder GameObject and activating it
             enemies[randomEnemyID].transform.SetParent(enabledEnemiesHolder.transform);
             enemies[randomEnemyID].SetActive(true);
 
+            // Removing the occurence from the List so that it is not activated twice
             enemies.RemoveAt(randomEnemyID);
         }
     }
