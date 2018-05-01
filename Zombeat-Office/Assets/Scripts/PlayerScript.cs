4250bb8a665f9db194bbdf7fc13f1e06de912f2f
Camille Briand <camille.briand.20160281@efrei.net>
2018-05-01 18:18:14 +0200

    Auto stash before merge of "dev_scripts" and "origin/dev_scripts"

--- Zombeat-Office/Assets/Scripts/PlayerScript.cs
+++ Zombeat-Office/Assets/Scripts/PlayerScript.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
-
+    
     private bool canMove = false;
 
     [HideInInspector] public bool actionPeriod;
     [HideInInspector] public bool hasMoved;
-
-
-	// Use this for initialization
+    
 	void Start () {
 		
 	}
