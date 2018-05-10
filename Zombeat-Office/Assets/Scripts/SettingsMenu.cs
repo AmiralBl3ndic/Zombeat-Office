eb58adcdac37acc123b9c17e8ae4178027451b1f
Unknown <noam.leipold.20160099@efrei.net>
2018-05-10 11:46:04 +0200

    Add more complex option menu

---
+++ Zombeat-Office/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetVolume( float volume )
+    {
+        mixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+    }
+
+}
